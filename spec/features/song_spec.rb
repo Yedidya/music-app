9cc9add71d8dd4dd5968fd8ac7b6c30ed83895bd
Yedidya Weiner <ydweiner17@gmail.com>
2015-03-11 15:56:40 -0500

    more tests for user sign up

--- spec/features/song_spec.rb
+++ spec/features/song_spec.rb
@@ -7,25 +7,36 @@ require 'capybara/rspec'
 describe "the signin process", :type => :feature do
   before :each do
     User.create(:email => 'user@example.com', :password => 'password', :username => 'Bob')
-  end
-
-  it "signs me in" do
     visit new_user_session_path
       fill_in 'Email', :with => 'user@example.com'
       fill_in 'Password', :with => 'password'
     click_button 'Sign in'
-    expect(page).to have_content 'Bob'
   end
 
-  it "creates a new user" do
+  it "signs me in" do
+    expect(page).to have_content 'Bob'
+  end
+end
+
+describe "the signup process", :type => :feature do
+  before :each do
     visit new_user_registration_path
       fill_in 'Choose a username', :with => 'Anthony'
       fill_in 'Email', :with => 'anthony@aol.com'
       fill_in 'Password', :with => 'password'
       fill_in 'Confirm Password', :with => 'password'
     click_button 'Sign Up'
-    expect(page).to have_content 'Anthony'
   end
 
 
+  it "creates a new user" do
+    User.last.username == 'Anthony'
+  end
+
+  it "leads us to the users home page" do
+    expect(page).to have_content('Anthony')
+  end
 end
+
+
+
